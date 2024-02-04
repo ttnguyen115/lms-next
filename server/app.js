2c823a35bf3eba6ecb54ed1be1a0da87a7ce81a7
ttnguyen115 <thanhtrung1012000@gmail.com>
2024-02-04 16:14:45 +0700

    fix: backend - update package version

--- server/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.app = void 0;
-var express_1 = require("express");
-exports.app = (0, express_1.default)();
--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,6 @@
 require("dotenv").config();
 import { app } from "./app";
 
-// create server
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
